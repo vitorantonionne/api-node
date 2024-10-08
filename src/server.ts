3494e0858871f595ebcdf0072ec89c9541ac37b1
vitorantonionne <vitor-antonionne@hotmail.com>
2024-10-08 19:18:15 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import fastify from 'fastify'
+
+
+const app = fastify()
+
+app.get('/hello', () => {
+  console.log('Hello Wordl');
+})
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('Http Runing Server');
+})
