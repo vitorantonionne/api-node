92e447e911e3a61c02aa84f051e193d4863f9c8b
vitorantonionne <vitor-antonionne@hotmail.com>
2024-10-09 19:24:18 -0300

    eslint

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import fastify from 'fastify'
 
-
 const app = fastify()
 
 app.get('/hello', () => {
-  console.log('Hello Wordl');
+  console.log('Hello Wordl')
 })
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('Http Runing Server');
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Http Runing Server')
+  })
