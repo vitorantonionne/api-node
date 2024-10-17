7ffa7ee2bd1314296dfbd24f0ee4061a295922d7
vitorantonionne <vitor-antonionne@hotmail.com>
2024-10-17 17:01:49 -0300

    validation env

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,9 +2,17 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
-  NODE_ENV:
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
   PORT: z.number().default(3333),
 })
 
-export const env = envSchema.parse(process.env)
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('Invalid environment variables', _env.error.format())
+
+  throw new Error('Invalid enviroment variables')
+}
+
+export const env = _env.data
